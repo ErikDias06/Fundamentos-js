79b2da6b42104609be9f6104fd014b230400eaeb
Erik Dias <erikhdias2006@gmail.com>
2026-02-13 14:58:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -437,6 +437,35 @@ function tabuada(numero) {
 }
 tabuada(numero);
 
+// ================================
+//    3  Desafios e Soluções
+// ================================
+
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = ['maçã', 'banana', 'morango', 'pêssego' ];
+console.log(listaGenerica);
+
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+console.log(linguagensDeProgramacao);
+
+// 3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagemDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagemDeProgramacao);
+
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomesDaAmigos = ['Pedro', 'Marcos','Gabriel'];
+console.log(nomesDaAmigos[0]);
+
+// 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let nomesDoAmigos = ['Pedro', 'Marcos','Gabriel'];
+console.log(nomesDoAmigos[1]);
+
+// 6. Crie uma lista com 3 nomes e exiba no console apenas o ultimo elemento.
+let nomesDeAmigos = ['Pedro', 'Marcos','Gabriel'];
+console.log(nomesDeAmigos[2]);
+
 
 
 
